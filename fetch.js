5a07202e78456e76db47a25c0f031e3e9c5366fa
lim-james <jameslimbj@gmail.com>
2018-09-06 17:29:13 +0800

    Added input field

--- fetch.js
+++ fetch.js
@@ -3,14 +3,14 @@ var next;
 function set(post) {
     $("#title a").text(post.title);
     $("#title a").attr("href", "https://www.reddit.com" + post.permalink);
-    $("#link").attr("href", post.url);
     $("#author a").text("u/" + post.author);
     $("#author a").attr("href", "https://www.reddit.com/u/" + post.author);
-    $("#image").attr("src", post.preview.images[0].source.url);
+    $("#image").attr("src", post.url);
 }
 
 function fetch() {
-    $.getJSON( "https://www.reddit.com/r/pewdiepiesubmissions.json", function( result ) {
+    console.log($("#sub").val());
+    $.getJSON( "https://www.reddit.com/r/" + $("#sub").val() + ".json", function( result ) {
         var count = result.data.dist;
         while (true) {
             var post = result.data.children[Math.floor(count * Math.random())].data;
